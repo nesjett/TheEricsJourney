a75359d1ecfe6883ef2887d10d2a986d5e20b57d
nesjett <web.nsabater@gmail.com>
2020-03-14 07:47:54 -0700

    Updated to include Engine

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <SFML/Graphics.hpp>
+//#include <SFML/Graphics.hpp>
 #include <iostream>
 
 #include "include/config.h"
-#include "src/public/game.h"
+#include "src/public/Engine.h" // INit the graphics engine
+#include "src/public/game.h" // Init the game instance
 
 #define kVel 5
 
