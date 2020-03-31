daadea7ace6ad853b2be788c2ba000d7229dc087
Kingmar616 <davidfuentesalonso17@gmail.com>
2020-03-31 18:17:09 +0200

    Otro push

--- src/private/PlayerController.cpp
+++ src/private/PlayerController.cpp
@@ -1,14 +1,13 @@
 #include "../public/PlayerController.h"
 
-#include <iostream>
-#include <list>
-#include <iterator>
-#include <SFML/Graphics.hpp>
-#include <algorithm>
 
 PlayerController::PlayerController(Player* jugador){
     stop = sf::Vector2f(0.0,0.0);
     miJugador = jugador;
+    pos1 = sf::Vector2f(0.0, -0.3);
+    pos2 = sf::Vector2f(0.0, +0.3);
+    pos3 = sf::Vector2f(-0.3, 0.0);
+    pos4 = sf::Vector2f(+0.3, 0.0);
 
 }
 
@@ -20,8 +19,8 @@ void PlayerController::Update(bool pulsada, sf::Keyboard::Key tecla){
             directionAux = miJugador->direction;
             miJugador->direction = directionAux;
         }*/
-        pos1 = sf::Vector2f(0.0, -0.3);
-        miJugador->Movimiento(pos1);
+        
+        miJugador->Movimiento(sf::Vector2f(0.0, -0.3));
     }
     if(tecla==sf::Keyboard::Down || tecla==sf::Keyboard::S){
         /*pos2 = sf::Vector2f(miJugador->direction.x,miJugador->direction.y+0.3);
@@ -30,8 +29,8 @@ void PlayerController::Update(bool pulsada, sf::Keyboard::Key tecla){
             directionAux = miJugador->direction;
             miJugador->direction = directionAux;
         }*/
-        pos2 = sf::Vector2f(0.0, +0.3);
-        miJugador->Movimiento(pos2);
+        
+        miJugador->Movimiento(sf::Vector2f(0.0, +0.3));
     }
     if(tecla==sf::Keyboard::Left || tecla==sf::Keyboard::A){
         /*pos3 = sf::Vector2f(miJugador->direction.x-0.3, miJugador->direction.y);
@@ -40,8 +39,8 @@ void PlayerController::Update(bool pulsada, sf::Keyboard::Key tecla){
             directionAux = miJugador->direction;
             miJugador->direction = directionAux;
         }*/
-        pos3 = sf::Vector2f(-0.3, 0.0);
-        miJugador->Movimiento(pos3);
+        
+        miJugador->Movimiento(sf::Vector2f(-0.3, 0.0));
     }
     if(tecla==sf::Keyboard::Right || tecla==sf::Keyboard::D){
         /*pos4 = sf::Vector2f(miJugador->direction.x+0.3, miJugador->direction.y);
@@ -50,8 +49,8 @@ void PlayerController::Update(bool pulsada, sf::Keyboard::Key tecla){
             directionAux = miJugador->direction;
             miJugador->direction = directionAux;
         }*/
-        pos4 = sf::Vector2f(+0.3, 0.0);
-        miJugador->Movimiento(pos4);
+        
+        miJugador->Movimiento(sf::Vector2f(+0.3, 0.0));
     }
     else{
         miJugador->Movimiento(stop);
