25dc6e6559f242fb8521c51132a6376d861ca70e
Kingmar616 <davidfuentesalonso17@gmail.com>
2020-04-01 02:38:53 +0200

    diagonal ok. Falta frenada.

--- src/private/PlayerController.cpp
+++ src/private/PlayerController.cpp
@@ -9,72 +9,37 @@ PlayerController::PlayerController(Player* jugador){
 
 void PlayerController::Update(sf::Keyboard::Key tecla, bool soltada){
     if(tecla==sf::Keyboard::Up || tecla==sf::Keyboard::W){
-        /*pos1 = sf::Vector2f(miJugador->direction.x,miJugador->direction.y-0.3);
-        miJugador->direction = pos1;
-        if (pulsada==false){
-            directionAux = miJugador->direction;
-            miJugador->direction = directionAux;
-        }*/
-        //miJugador->Movimiento(sf::Vector2f(0.f, -0.05));
         if(soltada == true){
-            miJugador->setDirection(miJugador->getDirection().x, miJugador->getDirection().y);
+            miJugador->setDirection(miJugador->getDirection().x, 0.f);
         }
         else{
-            miJugador->setDirection(0.f, (-0.05));
+            miJugador->setDirection(miJugador->getDirection().x, (-0.05));
         }
     }
     if(tecla==sf::Keyboard::Down || tecla==sf::Keyboard::S){
-        /*pos2 = sf::Vector2f(miJugador->direction.x,miJugador->direction.y+0.3);
-        miJugador->direction = pos2;
-        if (pulsada==false){
-            directionAux = miJugador->direction;
-            miJugador->direction = directionAux;
-        }*/
-        //miJugador->Movimiento(sf::Vector2f(0.f, +0.05));
         if(soltada == true){
             miJugador->setDirection(miJugador->getDirection().x, miJugador->getDirection().y);
         }
         else{
-            miJugador->setDirection(0.f, 0.05);
+            miJugador->setDirection(miJugador->getDirection().x, 0.05);
         }
     }
-    /*else{
-        miJugador->setDirection(0.f, 0.f);
-    }*/
     if(tecla==sf::Keyboard::Left || tecla==sf::Keyboard::A){
-        /*pos3 = sf::Vector2f(miJugador->direction.x-0.3, miJugador->direction.y);
-        miJugador->direction = pos3;
-        if (pulsada==false){
-            directionAux = miJugador->direction;
-            miJugador->direction = directionAux;
-        }*/
-        //miJugador->Movimiento(sf::Vector2f(-0.05, 0.f));
         if(soltada == true){
             miJugador->setDirection(miJugador->getDirection().x, miJugador->getDirection().y);
         }
         else{
-            miJugador->setDirection((-0.05), 0.f);
+            miJugador->setDirection((-0.05), miJugador->getDirection().y);
         }
     }
     if(tecla==sf::Keyboard::Right || tecla==sf::Keyboard::D){
-        /*pos4 = sf::Vector2f(miJugador->direction.x+0.3, miJugador->direction.y);
-        miJugador->direction = pos4;
-        if (pulsada==false){
-            directionAux = miJugador->direction;
-            miJugador->direction = directionAux;
-        }*/
-        //miJugador->Movimiento(sf::Vector2f(+0.05, 0.f));
         if(soltada == true){
             miJugador->setDirection(miJugador->getDirection().x, miJugador->getDirection().y);
         }
         else{
-            miJugador->setDirection(0.05, 0.f);
+            miJugador->setDirection(0.05, miJugador->getDirection().y);
         }
     }
-    /*else{
-        //miJugador->Movimiento(stop);
-        miJugador->setDirection(miJugador->getDirection().x, miJugador->getDirection().y);
-    }*/
 }
 
 PlayerController::~PlayerController() // Destructor
