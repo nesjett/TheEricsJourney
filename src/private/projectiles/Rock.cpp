9c6dfb9047f754db5e7db8a860528905b076a046
nesjett <web.nsabater@gmail.com>
2020-04-26 15:18:26 -0700

    added rock projectile

---
+++ src/private/projectiles/Rock.cpp
@@ -0,0 +1,70 @@
+#include "../../public/projectiles/Rock.h"
+
+
+
+Rock::Rock() : Projectile(){ // Use this to call to parent's contructor first
+    Init();
+}
+Rock::Rock(sf::Vector2f dir, sf::Vector2f pos) : Projectile(){
+    Init();
+    direction = dir;
+    setActorLocation(pos); 
+    activeAnim = Animations.find("IDLE")->second;
+}
+void Rock::Init(){
+    movementSpeed = 0.1;
+    damage = 20;
+
+    texture_file = "./resources/projectiles/Rock-512.png";
+    if(sprite){
+        delete sprite;
+    }
+    sprite = new SSprite(texture_file);
+    
+    sprite->setOrigin(85.33/2, 28/2);
+    sprite->setTextureRect( 0, 5 , 85.33, 28 );
+
+
+    Animation *tmpA;
+
+    //IDLE
+    tmpA = new Animation(sprite->getSpriteR(), 1500, true);
+    Animations.insert({"IDLE", tmpA});
+    tmpA->addFrame({sf::IntRect(0,0, 85.33,33)});
+    tmpA->addFrame({sf::IntRect(85.33,0,85.33,33)});
+    tmpA->addFrame({sf::IntRect(170.66,0,85.33,33)});
+    tmpA->addFrame({sf::IntRect(0,33, 85.33,33)});
+    tmpA->addFrame({sf::IntRect(85.33,33,85.33,33)});
+    tmpA->addFrame({sf::IntRect(170.66,33,85.33,33)});
+    
+}
+
+void Rock::Update(float delta){
+    float x = movementSpeed*direction.x*delta;
+    float y = movementSpeed*direction.y*delta;
+    x = getActorLocation().x + x;
+    y = getActorLocation().y + y;
+    
+    UpdateMovement(Vector2f(x,y));
+    
+
+    // std::atan2 uses y, x signs' for quadrant signification, unlike std::atan
+    // SFML's y-axis is flipped: flip our y-component
+    auto angleRads = std::atan2(-direction.y, -direction.x);
+    auto angleDegs = angleRads * 180.0 / M_PI;
+    sprite->setRotation(angleDegs);
+}
+
+
+void Rock::OnActorOverlap(Actor *otherActor){
+    //otherActor.TakeDamage(damage, "default");
+    if ( dynamic_cast<Pawn*>(otherActor) && dynamic_cast<Pawn*>(otherActor)->getFaction() == allie ) { // allie = player related things
+        otherActor->TakeDamage(damage, this, "PROJECTILE_X");
+    }
+}
+
+Rock::~Rock(){
+    //delete sprite;
+}
+
+
---
+++ src/public/projectiles/Rock.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <Actor.h>
+#include <Projectile.h>
+
+
+class Rock: public Projectile {
+    public:
+        Rock();
+        Rock(sf::Vector2f, sf::Vector2f);
+        ~Rock();
+
+        void Update(float delta);
+        void Init();
+        void OnActorOverlap(Actor *otherActor);
+
+    protected:
+    private:
+
+};
