a555c9034b1ff7c7a800b1aa9a8e1c12ea74aac5
nesjett <web.nsabater@gmail.com>
2020-05-07 04:51:51 -0700

    added comments

--- src/private/Engine.cpp
+++ src/private/Engine.cpp
@@ -122,11 +122,13 @@ sf::Vector2f SSprite::Draw(sf::Vector2f location, sf::Vector2f location_prev, do
     return sf::Vector2f(x,y);
 }
 
+/* Gets the sprite original Global Bounds, calculated by SFML */
 sf::FloatRect SSprite::getGlobalBounds() {
     
     return sfsprite.getGlobalBounds();
 }
 
+/* GEts the modified global bounds of this SSprite */
 sf::FloatRect SSprite::getBounds() {
     sf::FloatRect global = sfsprite.getGlobalBounds();
     if(GlobalBounds.width == 0.f) {
@@ -247,7 +249,6 @@ void SSprite::setScale(double x, double y){
 
 /* Modify the sprite bounds. If param > 0 && < 1, size is applied as a sacling percentage, if > 1, Its set as an absolute size */
 sf::FloatRect SSprite::setBounds(float newScale) {
-    sf::FloatRect current = sfsprite.getGlobalBounds();
     if(newScale <= 1) { // we are sending a scale
         GlobalBounds.height = GlobalBounds.height*newScale;
         GlobalBounds.width = GlobalBounds.width*newScale;
