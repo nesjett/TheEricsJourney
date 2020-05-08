f5b7ffef225dcc3070a89bab58f8f2931c9e430d
nesjett <web.nsabater@gmail.com>
2020-05-08 10:40:50 -0700

    Merge branch 'Amador_dev2' of https://github.com/nesjett/TheEricsJourney

--- src/private/Player.cpp
+++ src/private/Player.cpp
@@ -186,12 +186,11 @@ void Player::Draw(double percent, double delta ){
 }
 
 void Player::Update(float delta){
-    /*if(IsAlive()){
-        Pawn::Update( delta);
-    }*/
-
     Pawn::Update(delta);
+    SetTarget(this->FindClosestEnemy());
 }
+
+
 void Player::TakeDamage(float damage, Actor* dmgCauser, string damage_type){
     //std::cout << "Player toke damage!" << std::endl; 
     if(health_Current > 0&&GodMode==false){ // Only apply damage if the enemy is alive.
@@ -244,7 +243,7 @@ void Player::SetTarget(Enemy *NewTarget) {
     }
 }
 
-void Player::setLista(list<Enemy*> listaEnemigos){
+Enemy* Player::FindClosestEnemy(){
     float minDist = 0.f;
     sf::Vector2f posPlayer = getActorLocation();
     sf::Vector2f dirToEnemy_tmp = sf::Vector2f(0.f, 0.f);
@@ -253,10 +252,9 @@ void Player::setLista(list<Enemy*> listaEnemigos){
 
     Enemy *enemy = nullptr;
 
-
-    for (Enemy *enemigo : listaEnemigos){
+    for (Enemy *enemigo : game::Instance()->getAllEnemies()){
         posEnemy = enemigo->getActorLocation();
-        dirToEnemy_tmp = posEnemy-posPlayer;
+        dirToEnemy_tmp = (posEnemy-posPlayer);
         float aux=sqrt(pow(dirToEnemy_tmp.x, 2)+pow(dirToEnemy_tmp.y, 2)); //Esto es la longitud del vector
         if(minDist == 0.f){
             minDist = aux;
@@ -268,18 +266,18 @@ void Player::setLista(list<Enemy*> listaEnemigos){
             enemy = enemigo;
         }
     }
-    dir_unit=Vector2f(dirToEnemy.x/minDist,dirToEnemy.y/minDist);
-    SetTarget(enemy);
+    dir_unit=Vector2f(dirToEnemy.x/minDist,dirToEnemy.y/minDist); // This should not be here
 
-    if(!enemy) { // FInish executing as no eligible enemy found
-        return;
-    }
-
+    return enemy;
 }
 
 // Base to implement attacks. This should be on the base class and be overriden by the different enemies
 void Player::Attack(){
     
+    if(!Target){
+        return;
+    }
+
     sf::Vector2f posPlayer = getActorLocation();
     //que empiece aqui
     game *eng = game::Instance();
