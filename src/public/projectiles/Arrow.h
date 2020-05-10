38feeafcddeeb13d63760fcf60852a5cb1389222
nesjett <web.nsabater@gmail.com>
2020-05-10 03:09:28 -0700

    updated to normal arrow

--- src/public/projectiles/Arrow.h
+++ src/public/projectiles/Arrow.h
@@ -11,18 +11,10 @@ class Arrow: public Projectile {
         ~Arrow();
 
         void Update(float delta);
-        void Draw(double percent, double delta );
         void Init();
         void OnActorOverlap(Actor *otherActor);
 
     protected:
-        /* Max number of times it is allowed to bounce */
-        int MaxBounceCount = 0;
     private:
-        Actor* DirectionPrecheck(Vector2f loc, ObjectType type);
-        sf::RectangleShape movementTraceDebug;
         Actor *lastDamaged;
-
-        int BounceCount = 0; // current count of bounces
-
 };
